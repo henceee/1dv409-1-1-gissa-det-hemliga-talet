4d9631a30288b57d280fe6e95c75595da2a26914
henceee <hg222dv@student.lnu.se>
2015-11-17 16:03:29 +0100

    2 tests not passed :worried:
    Test Name: CheckCountProperty
    Test FullName:
    NumberGuessingGame.Tests.Models.SecretNumberTest.CheckCountProperty
    Test Source:
    C:\Users\Hence\Documents\GitHub\1dv409-1-1-gissa-det-hemliga-talet\NumberGuessingGame.MVC5\NumberGuessingGame.Tests\Models\SecretNumberTest.cs
    : line 201
    Test Outcome: Failed
    Test Duration: 0:00:00,0018135
    Result StackTrace: vid
    NumberGuessingGame.Tests.Models.SecretNumberTest.CheckCountProperty() i
    C:\Users\Hence\Documents\GitHub\1dv409-1-1-gissa-det-hemliga-talet\NumberGuessingGame.MVC5\NumberGuessingGame.Tests\Models\SecretNumberTest.cs:rad
    217
    Result Message: Assert.IsTrue failed. Egenskapen Count slutar inte att
    räknas upp efter att sju gissningar gjorts och ytterligare gissningar
    görs.
    
    Test Name: CheckMakeGuesskNoMoreGuesses
    Test FullName:
    NumberGuessingGame.Tests.Models.SecretNumberTest.CheckMakeGuesskNoMoreGuesses
    Test Source:
    C:\Users\Hence\Documents\GitHub\1dv409-1-1-gissa-det-hemliga-talet\NumberGuessingGame.MVC5\NumberGuessingGame.Tests\Models\SecretNumberTest.cs
    : line 119
    Test Outcome: Failed
    Test Duration: 0:00:00,0924372
    
    Result StackTrace: vid
    NumberGuessingGame.Tests.Models.SecretNumberTest.CheckMakeGuesskNoMoreGuesses()
    i
    C:\Users\Hence\Documents\GitHub\1dv409-1-1-gissa-det-hemliga-talet\NumberGuessingGame.MVC5\NumberGuessingGame.Tests\Models\SecretNumberTest.cs:rad
    132
    Result Message: Assert.IsTrue failed. MakeGuess returnerar inte
    Outcome.NoMoreGuesses då fler gissningar än sju görs.

--- NumberGuessingGame.MVC5/NumberGuessingGame/Models/SecretNumber.cs
+++ NumberGuessingGame.MVC5/NumberGuessingGame/Models/SecretNumber.cs
@@ -12,27 +12,35 @@ namespace NumberGuessingGame.Models
         private GuessedNumber _lastGuessedNumber;
         private int? _number;
         public static readonly int MaxNumberOfGuesses = 7;
+        private const int MinVal = 1;
+        private const int MaxVal = 100;
 
         public bool CanMakeGuess
         {
             get
             {
-                if(Count >= MaxNumberOfGuesses)
-                {
-                    return true;
-                }
-                return false;
-
+                return Count < MaxNumberOfGuesses && _lastGuessedNumber.Outcome != Outcome.Right;                
             }
         }
 
-        public int Count { get { return _guessedNumbers.Count; } }
+        public int Count
+        {   
+            get
+            {
+                return _guessedNumbers.Count;
+            }
+        }
 
-        public IList<GuessedNumber> GuessedNumbers { get { return _guessedNumbers.AsReadOnly(); } }
+        public IList<GuessedNumber> GuessedNumbers
+        {
+            get
+            {
+                return _guessedNumbers.AsReadOnly();
+            }
+        }
 
         public GuessedNumber LastGuessedNumber { get { return _lastGuessedNumber; } }
-
-        [Range(1,100,ErrorMessage ="Talet måste vara mellan 1-100")]
+        
         public int? Number 
         {
             get
@@ -44,48 +52,53 @@ namespace NumberGuessingGame.Models
                 _number = value;
             }
         }
-
-        //TODO: ADD cstror with 1 param
+        
         public SecretNumber()
         {
+            _guessedNumbers = new List<GuessedNumber>(MaxNumberOfGuesses);
             this.Initialize();
         }
             
 
         public void Initialize()
         {
-            _guessedNumbers = new List<GuessedNumber>();
-            _guessedNumbers.Capacity = 7;
-            Random rand = new Random();
-            _number = rand.Next(1, 100);
+            _guessedNumbers.Clear();
+            _number = new Random().Next(MinVal, MaxVal);
             _lastGuessedNumber.Number = null;
             _lastGuessedNumber.Outcome = Outcome.Indefinite;
         }
 
         public Outcome MakeGuess(int guess)
         {
-            if(_guessedNumbers.Exists(g =>g.Number == guess))
-            {
-                _lastGuessedNumber.Outcome = Outcome.OldGuess;
-            }
             if (!CanMakeGuess)
             {
                 _lastGuessedNumber.Outcome = Outcome.NoMoreGuesses;
-            }
-            if (guess > _number)    
+            }   
+            if (guess < MinVal || guess > MaxVal)
             {
-                _lastGuessedNumber.Outcome = Outcome.High;
+                throw new ArgumentOutOfRangeException();
             }
-            if (guess < _number)
+            if (_guessedNumbers.Exists(g =>g.Number == guess))
             {
-                _lastGuessedNumber.Outcome = Outcome.Low;
+                _lastGuessedNumber.Outcome = Outcome.OldGuess;
             }
-            if (guess == _number)
+            else
             {
-                _lastGuessedNumber.Outcome = Outcome.Right;
-            }
-            _lastGuessedNumber.Number = guess;
-            _guessedNumbers.Add(_lastGuessedNumber);
+                if (guess > _number)
+                {
+                    _lastGuessedNumber.Outcome = Outcome.High;
+                }
+                if (guess < _number)
+                {
+                    _lastGuessedNumber.Outcome = Outcome.Low;
+                }
+                if (guess == _number)
+                {
+                    _lastGuessedNumber.Outcome = Outcome.Right;
+                }
+                _lastGuessedNumber.Number = guess;
+                _guessedNumbers.Add(_lastGuessedNumber);
+            }            
 
             return _lastGuessedNumber.Outcome;
         }
